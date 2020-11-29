214de89564b07be9f7ba2fd588c89aaf3c6da269
VansKFC <74155424+VansKFC@users.noreply.github.com>
2020-11-30 07:38:36 +0900

    Update Main.java

--- Combat/Main.java
+++ Combat/Main.java
@@ -1,1 +1,15 @@
-public class Main {
+package combat.main; //Package location
+
+import org.bukkit.attribute.Attribute; //Bukkit Imports
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+//Main clas
+public class Main implements Listener {
+  @EventHandler //Tag as event handler.
+  public void onJoin(PlayerJoinEvent event) { //Make code run on join event.
+    Player player = event.getPlayer(); //Define player
+    player.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(16.0D); //Set attack speed attribute. 16+ to disable hit cooldown.
+  }
+}
