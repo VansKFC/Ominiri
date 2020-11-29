f42d10962ea1dbd0ae32f93d1d216230380231c2
VansKFC <74155424+VansKFC@users.noreply.github.com>
2020-11-30 08:58:52 +0900

    Create Main.java

---
+++ combat/main/Main.java
@@ -0,0 +1,11 @@
+package combat.main;
+import org.bukkit.plugin.java.JavaPlugin;
+public class Main extends JavaPlugin {
+    @Override //Enables plugin
+    public void onEnable(){
+    }
+    @Override
+    public void onDisable(){
+    }
+
+}
